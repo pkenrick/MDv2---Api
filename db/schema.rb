7e1e8b05e1a460db2a5fce8c77d59d156605e8ac
paulkenrick <pkenrick@gmail.com>
2018-01-21 17:32:43 +0000

    Adding notes to tasks.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030190641) do
+ActiveRecord::Schema.define(version: 20180121173127) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171030190641) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "list_type",   default: "private"
+    t.text     "notes"
   end
 
   add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
